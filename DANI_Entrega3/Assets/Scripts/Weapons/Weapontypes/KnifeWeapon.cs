e68a3e6f08e12549e0b85b56e12888bec86f10d7
PaulaBGM <borrasgarciap@gmail.com>
2026-05-24 17:10:27 +0200

    d

--- DANI_Entrega3/Assets/Scripts/Weapons/Weapontypes/KnifeWeapon.cs
+++ DANI_Entrega3/Assets/Scripts/Weapons/Weapontypes/KnifeWeapon.cs
@@ -2,18 +2,38 @@ using UnityEngine;
 
 public class KnifeWeapon : WeaponBase
 {
-    [SerializeField] private float range = 2f;
-    [SerializeField] private LayerMask enemyLayer;
+    [SerializeField]
+    private float range = 2f;
+
+    [SerializeField]
+    private LayerMask enemyLayer;
 
     public override void Fire()
     {
-        Ray ray = new Ray(Camera.main.transform.position, Camera.main.transform.forward);
+        Ray ray =
+            new Ray(
+                Camera.main.transform.position,
+                Camera.main.transform.forward);
 
-        if (Physics.Raycast(ray, out RaycastHit hit, range, enemyLayer))
+        if (Physics.Raycast(
+            ray,
+            out RaycastHit hit,
+            range,
+            enemyLayer))
         {
-            if (hit.collider.TryGetComponent(out IDamageable damageable))
+            Debug.Log(
+                $"KNIFE HIT: {hit.collider.name}");
+
+            IDamageable damageable =
+                hit.collider.GetComponentInParent<IDamageable>();
+
+            if (damageable != null)
             {
-                damageable.ApplyDamage(data.damage);
+                damageable.ApplyDamage(
+                    data.damage);
+
+                Debug.Log(
+                    $"KNIFE DAMAGE: {data.damage}");
             }
         }
     }
